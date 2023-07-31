b7cb7edebdee8bb322785fb94fbf1cd3a3b0be71
kayhan-kashi <kayhan.kashi@gmail.com>
2023-07-31 07:21:03 +0330

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
+  viewMode = 'map';
+  // courses = [1, 2];
+  courses = [
+    { id : 1, name:'course1' },
+    { id : 2, name:'course2' },
+    { id : 3, name:'course3' }
+  ]
+
+  coursesForBtn;
+
   title: string = 'Angular demo';
   post = {
     title: "Title",
@@ -24,6 +34,36 @@ export class AppComponent {
 
   onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
     console.log("Favorite changed", eventArgs.newValue)
+  }
+
+  changeViewMode(value: string) {
+    this.viewMode = value;
+  }
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4'})
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
+ 
+  loadCourses() {
+    this.coursesForBtn = [
+      { id : 1, name:'course1' },
+      { id : 2, name:'course2' },
+      { id : 3, name:'course3' }
+    ]
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined; 
+    // if we have a course object we want to return it's id otherwise we want to return undefined
   }
 
 }
